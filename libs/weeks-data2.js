2819ed5ca71c527213823919f5a0cb6e0e301d4c
Yasar <abuabdullah1@ymail.com>
2022-06-21 16:11:42 +0100

    dummy data prt 2

---
+++ libs/weeks-data2.js
@@ -0,0 +1,282 @@
+const weeks = {
+    week1: [
+      {
+        day: 1,
+        resouces: [
+          {
+            id: 301,
+            topic: 'HTML basics',
+            url: 'https://www.youtube.com/watch?v=qz0aGYrrlhU',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 101,
+            topic: 'HTML, elements and attributes ',
+            weekend:true,
+            completed:true,
+            comments: "great, very easy to follow"
+         },
+         {
+          id: 1012,
+          topic: 'HTML, class and ids',
+          weekend: true,
+          completed: true,
+          comments:"good, short and concise"
+       }
+        ]
+      },
+      {
+        day: 2,
+        resouces: [
+          {
+            id: 702,
+            topic: 'CSS, basics',
+            url: 'https://www.youtube.com/watch?v=1PnVor36_40',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 102,
+            topic: 'CSS syntax and selectors',
+            weekend: true,
+            completed: true,
+            comments:"just what I needed"
+            
+          },
+          {
+            id: 1022,
+            topic: 'backgrounds, borders and margins',
+            weekend: true,
+            completed:no,
+            comments:"I grasped the concepts well during class"
+            
+          }
+        ]
+      },
+      {
+        day: 3,
+        resouces: [
+          {
+            id: 803,
+            topic: 'Javascript intro',
+            url: 'https://www.youtube.com/watch?v=W6NZfCO5SIk&t=374s',
+            rating: false
+          
+          }
+        ],
+        list: [
+          {
+            id: 103,
+            topic: 'data types and variables',
+            weekend:true,
+            completed:true,
+            comments:"It was too technical. i would have liked a step by step guide"
+            
+          }
+        ] 
+      },
+      {
+        day: 4,
+        resouces: [
+          {
+            id: 904,
+            topic: 'javascript intro part 2',
+            url: 'https://www.youtube.com/watch?v=W6NZfCO5SIk&t=374s',
+            rating: false
+          
+          }
+        ],
+        list: [
+          {
+            id: 104,
+            topic: 'objects and arrays',
+            weekend:false,
+            completed:false,
+            comments:""
+            
+          },
+          {
+            id: 1042,
+            topic: 'loops and if statements',
+            weekend:false,
+            completed:true,
+            comments:"really cleared up the confusion I had"
+            
+          }
+        ]
+      },
+      {
+        day: 5,
+        resouces: [
+          {
+            id: 205,
+            topic: 'javascript continued',
+            url: 'https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 105,
+            topic: 'fetch',
+            weekend:false,
+            completed:true,
+            comments:"brilliant"
+            
+          },
+          {
+            id: 1052,
+            topic: 'change HTML elements',
+            weekend:true,
+            completed:true,
+            comments:"I would have prefered a practical"
+            
+          }
+        ]
+      },
+    ],
+    week2: [
+      {
+        day: 1,
+        resouces: [
+          {
+            id: 5406,
+            topic: 'intro to node',
+            url: 'https://www.youtube.com/watch?v=TlB_eWDSMt4&t=8s',
+            rating: true
+          
+          
+          }
+        ],
+        list: [
+          {
+            id: 106,
+            topic: 'node modules',
+            weekend:true,
+            completed:true,
+            comments:"still have no clue"
+          },
+          {
+          id: 1062,
+          topic: 'http modules',
+          weekend:true,
+          completed:true,
+          comments:"still lost"
+        }
+        ]
+      },
+      {
+        day: 2,
+        resouces: [
+          {
+            id: 6507,
+            topic: 'express',
+            url: 'https://www.section.io/engineering-education/express/',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 107,
+            topic: 'request handlers',
+            weekend:true,
+            completed:false,
+            comments:""
+            
+          }
+        ]
+      },
+      {
+        day: 3,
+        resouces: [
+          {
+            id: 508,
+            topic: 'databases',
+            url: 'https://www.youtube.com/watch?v=z2kbsG8zsLM',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 108,
+            topic:' postgres',
+            weekend:true,
+            completed:false,
+            comments:"not been feeling well, will complete later"
+            
+          }
+        ]
+      },
+      {
+        day: 4,
+        resouces: [
+          {
+            id: 409,
+            topic: 'Heroku',
+            url: 'https://www.youtube.com/watch?v=QTOkqzCTGxw',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 109,
+            topic: 'relational databases',
+            weekend:true,
+            completed:true,
+            comments:"hard to follow"
+            
+            
+          },
+          {
+            id: 1092,
+            topic: 'non relational databases',
+            weekend:true,
+            completed:true,
+            comments:"it was ok"
+            
+            
+          }
+        ]
+      },
+      {
+        day: 5,
+        resouces: [
+          {
+            id: 201,
+            topic: 'Restful Apis',
+            url: 'https://www.youtube.com/watch?v=lsMQRaeKNDk',
+            rating: true
+          
+          }
+        ],
+        list: [
+          {
+            id: 1001,
+            topic: 'CRUD operations',
+            weekend:true,
+            completed:true,
+            comments:"great"
+            
+          },
+          {
+            id: 10012,
+            topic: 'scaling operations',
+            weekend:true,
+            completed:true,
+            comments:"I t was very rushed, will need to re-visit"
+            
+          }
+        ]
+      }
+    ]
+  }
+  export default weeks
