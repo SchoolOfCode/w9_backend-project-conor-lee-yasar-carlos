3f8b7d6756a5f0981eb3f7601cee132b59065d91
Carlos E Alford <carlos.alford@gmail.com>
2022-06-21 10:15:23 +0100

    Add lib base object to be populated

---
+++ libs/weeks-data.js
@@ -0,0 +1,104 @@
+export const weeks = {
+  week1: [
+    {
+      day: 1,
+      resouces: [
+        {
+          id: 'number',
+          text: 'string',
+          rating: 'boolen'
+        }
+      ],
+      list: [
+        {
+          id: 'alpha',
+          content: 'string',
+          rating: 'number',
+          weekend: 'true',
+          completed: 'boolean',
+          comments: 'string'
+        }
+      ]
+    },
+    {
+      day: 2,
+      resouces: [
+        {
+          id: 'number',
+          text: 'string',
+          rating: 'boolen'
+        }
+      ],
+      list: [
+        {
+          id: 'alpha',
+          content: 'string',
+          rating: 'number',
+          weekend: 'true',
+          completed: 'boolean',
+          comments: 'string'
+        }
+      ]
+    },
+    {
+      day: 3,
+      resouces: [
+        {
+          id: 'number',
+          text: 'string',
+          rating: 'boolen'
+        }
+      ],
+      list: [
+        {
+          id: 'alpha',
+          content: 'string',
+          rating: 'number',
+          weekend: 'true',
+          completed: 'boolean',
+          comments: 'string'
+        }
+      ]
+    },
+    {
+      day: 4,
+      resouces: [
+        {
+          id: 'number',
+          text: 'string',
+          rating: 'boolen'
+        }
+      ],
+      list: [
+        {
+          id: 'alpha',
+          content: 'string',
+          rating: 'number',
+          weekend: 'true',
+          completed: 'boolean',
+          comments: 'string'
+        }
+      ]
+    },
+    {
+      day: 5,
+      resouces: [
+        {
+          id: 'number',
+          text: 'string',
+          rating: 'boolen'
+        }
+      ],
+      list: [
+        {
+          id: 'alpha',
+          content: 'string',
+          rating: 'number',
+          weekend: 'true',
+          completed: 'boolean',
+          comments: 'string'
+        }
+      ]
+    }
+  ]
+}
