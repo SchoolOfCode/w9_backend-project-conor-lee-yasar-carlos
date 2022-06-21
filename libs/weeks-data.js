d859a13291aa1967aeb8a10062254851bd867e6e
Yasar <abuabdullah1@ymail.com>
2022-06-21 13:54:05 +0100

    dummy data

--- libs/weeks-data.js
+++ libs/weeks-data.js
@@ -4,39 +4,42 @@ export const weeks = {
       day: 1,
       resouces: [
         {
-          id: 'number',
-          text: 'string',
-          rating: 'boolen'
+          id: 01,
+          topic: 'HTML basics',
+          url: 'https://www.youtube.com/watch?v=qz0aGYrrlhU'
+        
         }
       ],
       list: [
         {
-          id: 'alpha',
-          content: 'string',
-          rating: 'number',
-          weekend: 'true',
-          completed: 'boolean',
-          comments: 'string'
-        }
+          id: 101,
+          topic: 'HTML, elements and attributes '
+       },
+       {
+        id: 1012,
+        topic: 'HTML, class and ids'
+     }
       ]
     },
     {
       day: 2,
       resouces: [
         {
-          id: 'number',
-          text: 'string',
-          rating: 'boolen'
+          id: 02,
+          topic: 'CSS, basics',
+          url: 'https://www.youtube.com/watch?v=1PnVor36_40'
         }
       ],
       list: [
         {
-          id: 'alpha',
-          content: 'string',
-          rating: 'number',
-          weekend: 'true',
-          completed: 'boolean',
-          comments: 'string'
+          id: 102,
+          topic: 'CSS syntax and selectors'
+          
+        },
+        {
+          id: 1022,
+          topic: 'backgrounds, borders and margins'
+          
         }
       ]
     },
@@ -44,39 +47,38 @@ export const weeks = {
       day: 3,
       resouces: [
         {
-          id: 'number',
-          text: 'string',
-          rating: 'boolen'
+          id: 03,
+          topic: 'Javascript intro',
+          url: 'https://www.youtube.com/watch?v=W6NZfCO5SIk&t=374s'
         }
       ],
       list: [
         {
-          id: 'alpha',
-          content: 'string',
-          rating: 'number',
-          weekend: 'true',
-          completed: 'boolean',
-          comments: 'string'
+          id: 103,
+          topic: 'data types and variables',
+          
         }
-      ]
+      ] 
     },
     {
       day: 4,
       resouces: [
         {
-          id: 'number',
-          text: 'string',
-          rating: 'boolen'
+          id: 04,
+          topic: 'javascript intro part 2',
+          url: 'https://www.youtube.com/watch?v=W6NZfCO5SIk&t=374s'
         }
       ],
       list: [
         {
-          id: 'alpha',
-          content: 'string',
-          rating: 'number',
-          weekend: 'true',
-          completed: 'boolean',
-          comments: 'string'
+          id: 104,
+          topic: 'objects and arrays',
+          
+        },
+        {
+          id: 1042,
+          topic: 'loops and if statements',
+          
         }
       ]
     },
@@ -84,21 +86,125 @@ export const weeks = {
       day: 5,
       resouces: [
         {
-          id: 'number',
-          text: 'string',
-          rating: 'boolen'
+          id: 05,
+          topic: 'javascript continued',
+          url: 'https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch'
         }
       ],
       list: [
         {
-          id: 'alpha',
-          content: 'string',
-          rating: 'number',
-          weekend: 'true',
-          completed: 'boolean',
-          comments: 'string'
+          id: 105,
+          topic: 'fetch',
+          
+        },
+        {
+          id: 1052,
+          topic: 'change HTML elements',
+          
         }
       ]
-    }
+    },
+    week2: [
+      {
+        day: 1,
+        resouces: [
+          {
+            id: 06,
+            topic: 'intro to node',
+            url: 'https://www.youtube.com/watch?v=TlB_eWDSMt4&t=8s'
+          
+          }
+        ],
+        list: [
+          {
+            id: 106,
+            topic: 'node modules'
+         },
+         {
+          id: 1062,
+          topic: 'http modules'
+       }
+        ]
+      },
+      {
+        day: 2,
+        resouces: [
+          {
+            id: 07,
+            topic: 'express',
+            url: 'https://www.section.io/engineering-education/express/'
+          }
+        ],
+        list: [
+          {
+            id: 107,
+            topic: 'request handlers'
+            
+          }
+        ]
+      },
+      {
+        day: 3,
+        resouces: [
+          {
+            id: 08,
+            topic: 'databases',
+            url: 'https://www.youtube.com/watch?v=z2kbsG8zsLM'
+          }
+        ],
+        list: [
+          {
+            id: 108,
+            topic:' postgres'
+            
+          }
+        ]
+      },
+      {
+        day: 4,
+        resouces: [
+          {
+            id: 09,
+            topic: 'Heroku',
+            url: 'https://www.youtube.com/watch?v=QTOkqzCTGxw'
+          }
+        ],
+        list: [
+          {
+            id: 109,
+            topic: 'relational databases'
+            
+          },
+          {
+            id: 1092,
+            topic: 'non relational databases'
+            
+          }
+        ]
+      },
+      {
+        day: 5,
+        resouces: [
+          {
+            id: 001,
+            topic: 'Restful Apis',
+            url: 'https://www.youtube.com/watch?v=lsMQRaeKNDk'
+          }
+        ],
+        list: [
+          {
+            id: 1001,
+            topic: 'CRUD operations'
+            
+          },
+          {
+            id: 10012,
+            topic: 'scaling operations'
+            
+          }
+        ]
+      }
+    ]
+    
   ]
 }
