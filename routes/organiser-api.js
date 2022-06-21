8c0fe2bfc48e15630d6b4cfd9686d8770b363ebf
Carlos E Alford <carlos.alford@gmail.com>
2022-06-21 23:35:25 +0100

    Listen to PATCH requests for the resources

--- routes/organiser-api.js
+++ routes/organiser-api.js
@@ -3,7 +3,8 @@ import {
   getWeekData,
   createTaskComment,
   updateTaskComment,
-  updateTaskMeta
+  updateTaskMeta,
+  updateResourceMeta
 } from '../models/organiser-api.js';
 
 
@@ -20,7 +21,7 @@ organiserRouter.get('/:userID/:weekID', (req, res) => {
 });
 
 // listen for post requests
-organiserRouter.post('/:userID/:weekID/:day/comment/:taskID', (req, res) => {
+organiserRouter.post('/:userID/:weekID/:day/resource/:resourceID', (req, res) => {
   // object with success response
   const response = createTaskComment({...req.params, ...req.body});
   
@@ -73,6 +74,25 @@ organiserRouter.patch('/:userID/:weekID/:day/task/:taskID', (req, res) => {
     res.json({
       success: false,
       payload: 'Unable to update the task metadata'
+    });
+  }
+});
+
+// listen for PATCH request to update the resources metadata
+organiserRouter.patch('/:userID/:weekID/:day/resource/:resourceID', (req, res) => {
+  // object with success response
+  const response = updateResourceMeta({...req.params, ...req.body});
+
+  // let end user know the sucess of update the task meta
+  if (response.success) {
+    res.json({
+      sucess: true,
+      payload: 'Resource metadata updated successfully'
+    });
+  }else{
+    res.json({
+      success: false,
+      payload: 'Unable to update the resource metadata'
     });
   }
 });
