0e3c6f1a51ede92f31ff8b2944f8a778d28ab74f
Carlos E Alford <carlos.alford@gmail.com>
2022-06-21 21:06:46 +0100

    Add PATCH listener to update a task unique comment

--- routes/organiser-api.js
+++ routes/organiser-api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getWeekData,
   createTaskComment,
+  updateTaskComment
 } from '../models/organiser-api.js';
 
 
@@ -27,8 +28,10 @@ organiserRouter.post('/:userID/weeks/:weekID/:day/:taskID', (req, res) => {
   const day = req.params.day;
   const comment = req.body.comment;
 
+  // object with success response
   const response = createTaskComment(userId, week, day, taskId, comment);
   
+  // let the end user know the sucess of the post
   if (response.success) {
     res.json({
       success: true,
@@ -41,6 +44,31 @@ organiserRouter.post('/:userID/weeks/:weekID/:day/:taskID', (req, res) => {
     });
   }
 
+});
+
+// listen for PATCH request to update the task for the day
+organiserRouter.patch('/:userID/week/:weekID/day/:day/task/:taskID', (req, res) => {
+  const userId = req.params.userID;
+  const week = req.params.weekID;
+  const day = req.params.day;
+  const taskId = Number(req.params.taskID);
+  const comment = req.body.comment;
+
+  // object with success response
+  const response = updateTaskComment(userId, week, day, taskId, comment);
+
+  // let the end user know the sucess of the patch
+  if (response.success) {
+    res.json({
+      sucess: true,
+      payload: 'Comment sucessfully updated'
+    });
+  }else{
+    res.json({
+      success: false,
+      payload: 'Unable to update the task comment'
+    });
+  }
 });
 
 export default organiserRouter;
