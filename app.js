a8bc082715a4db1bf0ba6d9df1f9e9346f08d766
Carlos E Alford <carlos.alford@gmail.com>
2022-06-22 09:55:52 +0100

    Add cors to usage to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import organiserRouter from './routes/organiser-api.js'
 
 
@@ -7,6 +8,8 @@ const app = express();
 
 // filter the response stream
 app.use(express.json());
+// cors will allow communication with the frontend
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
