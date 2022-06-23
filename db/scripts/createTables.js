8e8f5e8507b1e3ea19a978d1ffe31d7e7bd18861
Carlos E Alford <carlos.alford@gmail.com>
2022-06-23 23:38:27 +0100

    Update createTables.js file to use only 4 tables

--- db/scripts/createTables.js
+++ db/scripts/createTables.js
@@ -1,7 +1,7 @@
 import {query} from '../index.js';
 
 // keeps track of all the tasks
-const createtTasks = `CREATE TABLE IF NOT EXIST tasks(
+const createTasks = `CREATE TABLE IF NOT EXISTS tasks(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   task TEXT,
   week INT,
@@ -15,10 +15,11 @@ async function createtTasksTable() {
 }
 
 // tracks all resources
-const createResources = `CREATE TABLE IF NOT EXIST resources(
+const createResources = `CREATE TABLE IF NOT EXISTS resources(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   resource TEXT,
-  task_id INT
+  task_id INT,
+  url TEXT
 )`;
 
 // query data to create table
@@ -28,9 +29,9 @@ async function createResourcesTable() {
 }
 
 // tracks all bootcampers
-const createUsers = `CREATE TABLE IF NOT EXIST users(
+const createUsers = `CREATE TABLE IF NOT EXISTS users(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  user TEXT
+  user_name TEXT
 )`;
 
 // query data to create table
@@ -39,39 +40,14 @@ async function createUsersTable() {
   return data;
 }
 
-// tracks all bootcampers comments for a specific task
-const createComments = `CREATE TABLE IF NOT EXIST comments(
-  task_id INT,
-  user_id INT,
-  comment TEXT
-)`;
-
-// query data to create table
-async function createCommentsTable() {
-  let data = await query(createComments);
-  return data;
-}
-
-// tracks all bootcampers feedback on specific resource
-const createResourceUser = `CREATE TABLE IF NOT EXIST resourceUser(
-  resource_id INT,
-  user_id INT,
-  rating BOOLEAN
-)`;
-
-// query data to create table
-async function createResourceUserTable() {
-  let data = await query(createResourceUser);
-  return data;
-}
-
 // tracks all bootcampers settings for specific task
-const createTaskUser = `CREATE TABLE IF NOT EXIST tastUser(
+const createTaskUser = `CREATE TABLE IF NOT EXISTS taskUser(
   task_id INT,
   user_id INT,
   rating INT,
   is_weekend BOOLEAN,
-  completed BOOLEAN
+  completed BOOLEAN,
+  comments TEXT
 )`;
 
 // query data to create table
@@ -84,6 +60,4 @@ async function createTaskUserTable() {
 createtTasksTable();
 createResourcesTable();
 createUsersTable();
-createCommentsTable();
-createResourceUserTable();
 createTaskUserTable();
